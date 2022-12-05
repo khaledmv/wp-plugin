d9218f475199e6d9fb59ac4ee2d9799b0acebbbc
khaledmv <khaledhossionmv@gmail.com>
2022-12-05 14:54:21 +0600

    Add redme.txt file

--- myplugin.php
+++ myplugin.php
@@ -1,2 +1,21 @@
 <?php
+/*
+Plugin Name:  MyPlugin
+Description:  Example plugin for the video tutorial series, "WordPress: Plugin Development", available at webaiid.com.
+Plugin URI:   https://profiles.wordpress.org/myplugin
+Author:       Khaled
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /lang
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
 
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
