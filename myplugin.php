bece8c3ec3598f6286588e13bc17cb7e557ff124
khaledmv <khaledhossionmv@gmail.com>
2022-12-05 15:23:59 +0600

    initialize sublevel menu

--- myplugin.php
+++ myplugin.php
@@ -19,3 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 
 }
+
+
+
+if(is_admin()){
+	
+
+// include plugin dependencies
+require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
+require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+
+}
