f07996879dbdd087d4e17fb3d4dd0c2b4879266c
khaledmv <khaledhossionmv@gmail.com>
2022-12-05 15:49:43 +0600

    Initialized register settings-page

--- myplugin.php
+++ myplugin.php
@@ -28,5 +28,12 @@ if(is_admin()){
 // include plugin dependencies
 require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
 require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+require_once plugin_dir_path( __FILE__ ) . 'admin/settings-register.php';
+require_once plugin_dir_path( __FILE__ ) . 'admin/settings-callbacks.php';
 
 }
+
+
+
+
+
