b208bbf7d8d26712ac442a2475987d335fa239f9
khaledmv <khaledhossionmv@gmail.com>
2022-12-05 21:29:51 +0600

    Add callback input field

--- myplugin.php
+++ myplugin.php
@@ -33,7 +33,21 @@ require_once plugin_dir_path( __FILE__ ) . 'admin/settings-callbacks.php';
 
 }
 
+// default plugin options
+function myplugin_options_default() {
 
+	return array(
+		'custom_url'     => 'https://wordpress.org/',
+		'custom_title'   => 'Powered by WordPress',
+		'custom_style'   => 'disable',
+		'custom_message' => '<p class="custom-message">My custom message</p>',
+		'custom_footer'  => 'Special message for users',
+		'custom_toolbar' => false,
+		'custom_scheme'  => 'default',
+	);
+
+}
+ 
 
 
 
